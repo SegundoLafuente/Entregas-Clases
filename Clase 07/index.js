a10d0f01df15a0d55f430b6ce38671c66db7641f
SegundoLafuente <slafuentesegura@udesa.edu.ar>
2025-10-17 10:25:36 -0300

    Entrega Clase 1 Segunda Parte Ej 1

---
+++ Clase 07/index.js
@@ -0,0 +1,71 @@
+function rectangulo (altura, ancho){
+    let area = ancho * altura;
+    return area 
+}
+
+let area = rectangulo(2,2);
+console.log(area) 
+
+function triangulo (base, altura){
+    let area = base * altura / 2;
+    return area
+}
+
+let area = triangulo(2,2);
+console.log(area)
+
+function dolarHoy (pesos){
+    let dolares = pesos * 1455;
+    return dolares
+}
+
+let dolares = dolarHoy(2000);
+console.log(dolares)
+
+function precioFinal (precio){
+    let final = precio * 0.21;
+    return final
+}
+
+let final = precio(1000);
+console.log(final)
+
+function mitad (num){
+    let miti = num / 2;
+    return miti
+}
+
+let miti = mitad(4);
+console.log(miti);
+
+function suma (num1,num2){
+    let suma = num1 + num2;
+    return suma
+}
+
+let suma = sum(2,2);
+console.log(suma);
+
+function resta (num1,num2){
+    let resta = num1 - num2;
+    return resta
+}
+
+let resta = res(2,2);
+console.log(resta)
+
+function multiplicacion (num1,num2){
+    let cuenta = num1 * num2;
+    return cuenta
+}
+
+let cuenta = multiplicacion(2,2);
+console.log(cuenta);
+
+function division (num1,num2){
+    let division = num1 / num2;
+    return division
+}
+
+let division = div(4,2);
+console.log(division);
